e45a46377f4ed6367e29a2b568e12ec67df7093e
Dennis Crissman <dcrissma@redhat.com>
2017-12-15 08:48:17 -0500

    fix test packages

--- src/test/java/org/esbtools/auth/CachedRolesProviderTest.java
+++ src/test/java/org/esbtools/auth/CachedRolesProviderTest.java
@@ -16,7 +16,7 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.redhat.auth;
+package org.esbtools.auth;
 
 import org.esbtools.auth.util.CachedRolesProvider;
 import org.esbtools.auth.util.RolesCache;
