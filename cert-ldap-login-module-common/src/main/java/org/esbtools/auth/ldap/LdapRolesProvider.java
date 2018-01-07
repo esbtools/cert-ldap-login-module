ab7226b62d9b92f0e46bd9fba0f9535100f698aa
Alec Henninger <alechenninger@gmail.com>
2018-01-06 21:08:54 -0500

    Revert "Experiment with failFast flag"
    This reverts commit fee195e518d2472f5fe7beee15dcfaefc87fef5f.

--- cert-ldap-login-module-common/src/main/java/org/esbtools/auth/ldap/LdapRolesProvider.java
+++ cert-ldap-login-module-common/src/main/java/org/esbtools/auth/ldap/LdapRolesProvider.java
@@ -40,7 +40,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLSocketFactory;
-import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -69,15 +68,7 @@ public class LdapRolesProvider implements RolesProvider {
     // Connection pool needs to be a singleton
     private LDAPConnectionPool connectionPool;
 
-    private final SSLSocketFactory socketFactory;
-    private final LDAPConnectionOptions options;
-
     public LdapRolesProvider(String searchBase, LdapConfiguration ldapConfiguration) throws Exception {
-        this(searchBase, ldapConfiguration, true);
-    }
-
-    public LdapRolesProvider(String searchBase, LdapConfiguration ldapConfiguration,
-            boolean failFast) throws Exception {
         LOGGER.debug("Creating esbtoolsLdapRoleProvider");
 
         Objects.requireNonNull(searchBase);
@@ -85,22 +76,12 @@ public class LdapRolesProvider implements RolesProvider {
 
         this.searchBase = searchBase;
         this.ldapConfiguration = ldapConfiguration;
-        this.options = getConnectionOptions(ldapConfiguration);
-        this.socketFactory = getSocketFactory();
 
-        try {
-            this.connectionPool = connectAndStartPool();
-        } catch (LDAPException e) {
-            if (failFast) {
-                throw e;
-            }
-
-            LOGGER.error("Initial connection to LDAP failed, will attempt to reconnect");
-        }
+        initializeFromConfiguration();
     }
 
-    private static LDAPConnectionOptions getConnectionOptions(LdapConfiguration ldapConfiguration) {
-        LDAPConnectionOptions options = new LDAPConnectionOptions();
+    private void initializeFromConfiguration() throws Exception {
+
         if (ldapConfiguration.isDebug()) {
             // bridge java.util.Logger output to log4j
             System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
@@ -111,6 +92,10 @@ public class LdapRolesProvider implements RolesProvider {
             System.setProperty("com.unboundid.ldap.sdk.debug.type", DebugType.getTypeNameList());
         }
 
+        LDAPConnection ldapConnection;
+
+        LDAPConnectionOptions options = new LDAPConnectionOptions();
+
         // A value which specifies the maximum length of time in milliseconds that an attempt to establish a connection should be allowed to block before failing. By default, a timeout of 60,000 milliseconds (1 minute) will be used.
         options.setConnectTimeoutMillis(ldapConfiguration.getConnectionTimeoutMS());
         // A value which specifies the default timeout in milliseconds that the SDK should wait for a response from the server before failing. By default, a timeout of 300,000 milliseconds (5 minutes) will be used.
@@ -118,39 +103,33 @@ public class LdapRolesProvider implements RolesProvider {
         // A flag that indicates whether to use the SO_KEEPALIVE socket option to attempt to more quickly detect when idle TCP connections have been lost or to prevent them from being unexpectedly closed by intermediate network hardware. By default, the SO_KEEPALIVE socket option will be used.
         options.setUseKeepAlive(ldapConfiguration.isKeepAlive());
 
-        return options;
-    }
 
-    private SSLSocketFactory getSocketFactory() throws GeneralSecurityException {
         if(ldapConfiguration.getUseSSL()) {
             TrustStoreTrustManager trustStoreTrustManager = new TrustStoreTrustManager(
-                ldapConfiguration.getTrustStore(),
-                ldapConfiguration.getTrustStorePassword().toCharArray(),
-                "JKS",
-                true);
-            return new SSLUtil(trustStoreTrustManager).createSSLSocketFactory();
-        }
+                    ldapConfiguration.getTrustStore(),
+                    ldapConfiguration.getTrustStorePassword().toCharArray(),
+                    "JKS",
+                    true);
+            SSLSocketFactory socketFactory = new SSLUtil(trustStoreTrustManager).createSSLSocketFactory();
 
-        return null;
-    }
-
-    private LDAPConnectionPool connectAndStartPool() throws LDAPException {
-        LDAPConnection ldapConnection;
-
-        if (socketFactory != null && ldapConfiguration.getUseSSL()) {
             ldapConnection = new LDAPConnection(
-                socketFactory,
-                options,
-                ldapConfiguration.getServer(),
-                ldapConfiguration.getPort()
+                    socketFactory,
+                    options,
+                    ldapConfiguration.getServer(),
+                    ldapConfiguration.getPort(),
+                    ldapConfiguration.getBindDn(),
+                    ldapConfiguration.getBindDNPwd()
             );
         } else {
             LOGGER.warn("Not using SSL to connect to ldap. This is very insecure - do not use in prod environments!");
 
             ldapConnection = new LDAPConnection(
-                options,
-                ldapConfiguration.getServer(),
-                ldapConfiguration.getPort());
+                    options,
+                    ldapConfiguration.getServer(),
+                    ldapConfiguration.getPort(),
+                    ldapConfiguration.getBindDn(),
+                    ldapConfiguration.getBindDNPwd()
+            );
         }
 
         BindRequest bindRequest = new SimpleBindRequest(ldapConfiguration.getBindDn(), ldapConfiguration.getBindDNPwd());
@@ -161,7 +140,7 @@ public class LdapRolesProvider implements RolesProvider {
             throw new LDAPException(bindResult.getResultCode(), "Error binding to LDAP");
         }
 
-        LDAPConnectionPool connectionPool = new LDAPConnectionPool(
+        connectionPool = new LDAPConnectionPool(
             ldapConnection,
             /* initialConnections */ ldapConfiguration.getPoolSize() / 2,
             ldapConfiguration.getPoolSize(),
@@ -172,20 +151,10 @@ public class LdapRolesProvider implements RolesProvider {
         LOGGER.info("Initialized LDAPConnectionPool: poolSize={}, poolMaxAge={}, connectionTimeout={}, responseTimeout={}, debug={}, keepAlive={}.",
                 ldapConfiguration.getPoolSize(), ldapConfiguration.getPoolMaxConnectionAgeMS(), ldapConfiguration.getConnectionTimeoutMS(), ldapConfiguration.getResponseTimeoutMS(),
                 ldapConfiguration.isDebug(), ldapConfiguration.isKeepAlive());
-
-        return connectionPool;
     }
 
     @Override
     public Set<String> getUserRoles(String username) throws Exception {
-        if (connectionPool == null) {
-            synchronized (this) {
-                if (connectionPool == null) {
-                    connectAndStartPool();
-                }
-            }
-        }
-
         LOGGER.debug("getRoles("+username+")");
 
         Objects.requireNonNull(username);
